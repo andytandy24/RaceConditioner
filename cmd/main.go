d4a2e7c6dd1508d679aaeaf2c47ac3719d3e7126
andrtand <andytandy24@gmail.com>
2024-12-16 21:34:38 +0100

    Added main file and internal package

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	fortnite "github.com/andytandy24/internal"
+	"github.com/google/uuid"
+)
+
+func main() {
+	fmt.Println(fortnite.GLOBALVAR)
+	fmt.Println(uuid.New())
+}
