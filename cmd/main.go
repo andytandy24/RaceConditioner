ded6b48f920111f4cfa91f530fc4d767fb567d33
andytandy24 <andytandy24@gmail.com>
2024-12-16 23:09:06 +0100

    Added funtionality for the script and CLI flags for easy CLI usage

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"sync"
+	"time"
 
-	fortnite "github.com/andytandy24/internal"
-	"github.com/google/uuid"
+	httpRequester "github.com/andytandy24/internal"
 )
 
 func main() {
-	fmt.Println(fortnite.GLOBALVAR)
-	fmt.Println(uuid.New())
+	var wg sync.WaitGroup
+
+	// CLI flags
+	url := flag.String("u", "http://127.0.0.1", "Specifies the target url")
+
+	method := flag.String("X", "GET", "Specifies the http method that should be used")
+
+	numRequests := flag.Int("n", 20, "Defines the amount of simultaneous requests sent to a target")
+
+	data := flag.String("d", "", "Specifies the data that is meant to be sent")
+
+	debug := flag.Bool("D", false, "Toggles debug mode, is false by default")
+
+	flag.Parse()
+
+	// Requests URL
+	for i := 0; i < *numRequests; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			httpRequester.MakeRequest(*method, *url, *data)
+
+			if *debug {
+				fmt.Println("Completed at:", time.Now().String())
+			}
+		}()
+	}
+	wg.Wait()
 }
--- internal/main.go
+++ internal/main.go
@@ -1,3 +1,22 @@
 package internal
 
-var GLOBALVAR = 1
+import (
+	"log"
+	"net/http"
+)
+
+func MakeRequest(method string, url string, data string) {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		log.Fatalf("Failed to create http request: %s", err)
+	}
+
+	if data != "" {
+		//TBD
+	}
+
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("Failed to send http request: %s", err)
+	}
+}
