1ea2efff99d32994ea5501d63a2c4fed07f3cdb4
andytandy24 <andytandy24@gmail.com>
2024-12-17 20:03:37 +0100

    Updated the setup to work

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
-go build ./cmd/main.go -o RaceC
-sudo mv RaceC $PATH
+#!/bin/bash
+
+# Build the binary
+echo "Building the binary..."
+go build -o RaceC ./cmd/main.go
+
+# Move to /usr/local/bin
+echo "Moving binary to /usr/local/bin/"
+sudo mv ./RaceC /usr/local/bin/
+
+echo "Installation complete!"
